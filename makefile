6c8e3b1fc76f01447a3477d29ba532000e4017ad
Chris Rice <crice@mines.edu>
2014-11-17 20:31:08 -0700

    added command line arguments for windowed and fullscreen mode

--- makefile
+++ makefile
@@ -1,2 +1,4 @@
-all:
+mac:
 	g++ src/*.cpp -o glfw_test -framework OpenGl -framework CoreFoundation -I/usr/local/include -lglfw3 -lglew -lSOIL
+linux:
+	# no one knows
--- src/glfw_test.cpp
+++ src/glfw_test.cpp
@@ -25,16 +25,36 @@
 
 //////////////////////////
 // Function prototypes
-GLFWwindow* initializeGLFWWindow(int, int);
+GLFWwindow* initializeGLFWWindow(int, int, bool);
 
 //////////////////////////
 // Implementation
 int main(int argc, char* argv[]) {
     srand(time(NULL));
 
-    float width = 1920.0f;
-    float height = 1080.0f;
-    GLFWwindow* window = initializeGLFWWindow(width, height);
+    float width;
+    float height;
+    bool fullscreen;
+    
+    if (argc == 2 && std::string(argv[1]) == "-f"){
+        width  = 1366;
+        height = 768;
+        printf("Loading fullscreen with resolution: %f by %f", width, height);
+        fullscreen = true;
+    } else if (argc == 4 && std::string(argv[1]) == "-w") {
+        sscanf(argv[2], "%f", &width);
+        sscanf(argv[3], "%f", &height);
+        fullscreen = false;
+    } else {
+        printf("\nCommand line options:\n");
+        printf("\t-f\n");
+        printf("\t\tRun in fullscreen mode\n");
+        printf("\t-w <width> <height>\n");
+        printf("\t\tRun in windowed mode with width and height\n\n");
+        return 1;
+    }
+
+    GLFWwindow* window = initializeGLFWWindow(width, height, fullscreen);
 
     // Zooming stuff
     float field_of_view = 45.0f;
@@ -73,7 +93,6 @@ int main(int argc, char* argv[]) {
             drawables.push_back(Drawable(&cube, glm::vec3(i, 0.05f, j - 4.0f)));
         }
     }
-
 
     // Display loop
     while(!glfwWindowShouldClose(window)) {
@@ -174,7 +193,7 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-GLFWwindow* initializeGLFWWindow(int width, int height){
+GLFWwindow* initializeGLFWWindow(int width, int height, bool fullscreen){
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -185,10 +204,17 @@ GLFWwindow* initializeGLFWWindow(int width, int height){
     // Set up the MSAA level 
     glfwWindowHint(GLFW_SAMPLES, 16);
 
-    // Windowed
-    // GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", nullptr, nullptr); 
-    // Fullscreen 
-    GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", glfwGetPrimaryMonitor(), nullptr);
+    GLFWwindow* window;
+
+    if (fullscreen){
+        // Fullscreen 
+        window = glfwCreateWindow(width, height, "OpenGL", glfwGetPrimaryMonitor(),
+            nullptr);
+
+    } else {
+        // Windowed
+        window = glfwCreateWindow(width, height, "OpenGL", nullptr, nullptr); 
+    }
 
     // Hide the mouse
     // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
