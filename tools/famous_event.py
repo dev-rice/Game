90c27d4bbf8f76bbccaef9237ac8847d7e29a907
Chris Rice <crice@mines.edu>
2014-12-16 17:45:35 -0700

    Now you don't have to have beautiful soup or urllib2 to build the code.

--- tools/famous_event.py
+++ tools/famous_event.py
@@ -1,7 +1,9 @@
-from BeautifulSoup import BeautifulSoup 
-import urllib2
 import random
 import sys
+
+other_events = ["The Zerg Swarms finally conquer Texas.",
+                "Humans discover Middle Earth.",
+                "The 2048 bit computer is invented."]
 
 def getListItemsFromList(list_item):
     list_items = list_item.findAll('li')
@@ -18,24 +20,30 @@ def getListItemsFromList(list_item):
 
     return list_contents
 
+def getRandomEvent():
+    
+    return random.choice(other_events)
+
 if len(sys.argv) == 2:
     year = int(sys.argv[1])
     random_event = ""
-    if (year < 2600):
-        response = urllib2.urlopen('http://en.wikipedia.org/wiki/' + str(year))
-        html = response.read()
+    try:
+        import urllib2
+        from BeautifulSoup import BeautifulSoup 
+        if (year < 2600):
+            response = urllib2.urlopen('http://en.wikipedia.org/wiki/' + str(year))
+            html = response.read()
 
-        soup = BeautifulSoup(html)
+            soup = BeautifulSoup(html)
 
-        headers = soup.findAll('ul')
-        random_header = random.choice(headers)
+            headers = soup.findAll('ul')
+            random_header = random.choice(headers)
 
-        list_contents = getListItemsFromList(random_header)
-        random_event = random.choice(list_contents)
-    else:
-        other_events = [ "The Zerg Swarms finally conquer Texas.",
-                         "Humans discover Middle Earth.",
-                         "The 2048 bit computer is invented."]
-        random_event = random.choice(other_events)
+            list_contents = getListItemsFromList(random_header)
+            random_event = random.choice(list_contents)
+        else:
+            random_event = getRandomEvent()
+    except ImportError:
+        random_event = getRandomEvent()
 
     print "In the year \033[94m%d\033[0m: %s\n" % (year, random_event)
