50729b4c455a3114621784d69a1f89391d7d7815
Chris Rice <chrisrice93@gmail.com>
2014-12-30 21:18:59 -0700

    Added the dumbest class ever, profile.h. It is responsible for keeping global settings like anti aliasing level.

---
+++ src/profile.h
@@ -0,0 +1,13 @@
+// Dumb container static class for handling global options in
+// different places.
+
+#ifndef Profile_h
+#define Profile_h
+
+class Profile {
+public:
+    static bool fxaa;
+    static int fxaa_level;
+};
+
+#endif
