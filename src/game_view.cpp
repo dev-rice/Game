164e9ac685b1cd833b92b7e91bf47e43bd221597
Chris Rice <chrisrice93@gmail.com>
2014-12-30 20:03:20 -0700

    Enabled 4x FXAA by default. A few optimizations still need to be done in the FXAA algorithm.

--- src/game_view.cpp
+++ src/game_view.cpp
@@ -28,6 +28,11 @@ void GameView::update(){
     // Render the level to the framebuffer
     framebuffer->setAsRenderTarget();
     level->draw();
+    // Draw the framebuffer 3 times here, then once to the screen.
+    // This makes for a total of 4 FXAA passes (4x FXAA).
+    framebuffer->draw();
+    framebuffer->draw();
+    framebuffer->draw();
 
     // Draw the framebuffer
     screen->setAsRenderTarget();
