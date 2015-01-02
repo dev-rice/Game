d6d235650dd362f776f0244dcdddb6ac4c0c0489
Chris Rice <chrisrice93@gmail.com>
2015-01-02 00:01:53 -0700

    World uses the new window.

--- src/world.cpp
+++ src/world.cpp
@@ -1,12 +1,10 @@
 #include "world.h"
 
-World::World(GLFWwindow* window){
-    this->window = window;
-    addLevel("res/maps/aatest.map");
-}
+World::World(Window* window) : World(window, "res/maps/aatest.map"){}
 
-World::World(GLFWwindow* window, const char* level_filename){
+World::World(Window* window, const char* level_filename){
     this->window = window;
+    this->glfw_window = window->getGLFWWindow();
     addLevel(level_filename);
 }
 
@@ -16,8 +14,8 @@ World::~World(){
 
 void World::addLevel(const char* filename){
     Debug::info("Setting the world level to '%s'\n", filename);
-    level = new Level(window, filename);
-    game_view = new GameView(window, level);
+    level = new Level(glfw_window, filename);
+    game_view = new GameView(glfw_window, level);
 }
 
 void World::update(){
