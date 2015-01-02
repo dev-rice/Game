0ef2486fe81a7bb18906bb490b14133cc39fca19
twestpha <twestpha@email.com>
2015-01-01 22:45:31 -0700

    Removed print statement. Accidentally left it in earlier

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -16,7 +16,6 @@ Mouse::Mouse(FlatMesh* flat_mesh, GLFWwindow* window, GLuint shader_program, GLu
 glm::vec2 Mouse::getPosition(){
     double x;
     double y;
-    printf("Mouse position: %f, %f\n", x, y);
     glfwGetCursorPos(window, &x, &y);
 
     glm::vec3 gl_mouse_position = glm::vec3(x, y, 1.0) * glm::inverse(mouse_projection);
