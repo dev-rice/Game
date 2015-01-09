de27dd59cca3a9f40ee9477b66db120ebe306798
twestpha <twestpha@mines.edu>
2015-01-09 09:36:38 -0700

    We're good to go, pugi xml doesn't fuck up like rapidxml did

--- src/ui_window.cpp
+++ src/ui_window.cpp
@@ -13,6 +13,25 @@ void UIWindow::loadFromXML(std::string filepath){
     pugi::xml_parse_result result = doc.load_file(filepath.c_str());
 
     Debug::info("Parsed layout: %s\n", filepath.c_str());
+
+    pugi::xml_node layout_node = doc.child("layout");
+
+    if(strcmp(layout_node.child_value("type"), "ui_window") != 0){
+        Debug::error("Mismatched layout types. Expected ui_window, received %s\n", layout_node.child_value("type"));
+        return;
+    }
+
+    pugi::xml_node node = layout_node.child("type");
+    // printf("%s", node.name());
+    while(true){
+        printf("%s\n", node.name());
+
+        node = node.next_sibling();
+
+        if(!node){
+            break;
+        }
+    }
 }
 
 void UIWindow::draw(){
