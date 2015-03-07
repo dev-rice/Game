44b7ed2d4a99b3b914133e1ff558790f49886466
Chris Rice <chrisrice93@gmail.com>
2015-03-06 23:23:20 -0700

    Added font box debugging to the text shader.

--- shaders/text.fs
+++ shaders/text.fs
@@ -7,7 +7,17 @@ out vec4 outColor;
 uniform sampler2D base_texture;
 uniform vec3 textColor;
 
+const bool TEXT_DEBUG = false;
+
 void main() {
     vec4 base = texture(base_texture, Texcoord);
-    outColor = vec4(1.0, 1.0, 1.0, base.r) * vec4(textColor, 1.0);
+
+    if (TEXT_DEBUG){
+        // Debug
+        outColor = vec4(0.0, 0.0, 0.0, 1.0) + vec4(base.r, base.r, base.r, 0.0) * vec4(textColor, 1.0);
+    } else {
+        // Regular
+        outColor = vec4(1.0, 1.0, 1.0, base.r) * vec4(textColor, 1.0);
+    }
+
 }
