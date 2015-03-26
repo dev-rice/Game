3825b26468d2634d160d3577c61dfb37fd3fcfe4
Chris Rice <chrisrice93@gmail.com>
2015-03-26 10:37:10 -0600

    Usability changes to edit mode.

--- src/game_view_edit.cpp
+++ src/game_view_edit.cpp
@@ -16,29 +16,31 @@ void GameViewEdit::drawOtherStuff(){
     GLuint paint_texture = current_layer.getDiffuse();
     current_paint->attachTexture(paint_texture);
     current_paint->setPixelCoordinates(20, 220, 120, 320);
-    fancy_text->print(20, 180, "Paint: M");
-    fancy_text->print(20, 200, "Erase: N");
+    fancy_text->print(20, 180, "Paint: LMB");
+    fancy_text->print(20, 200, "Erase: RMB");
     fancy_text->print(30, 230, "%d", current_layer.getLayerNumber());
 }
 
 void GameViewEdit::handleInputState(){
-    GameView::handleInputState();
+    // GameView::handleInputState();
 
     const Uint8 *state = SDL_GetKeyboardState(NULL);
     // Get the mouse coordinates gl, and the world
     glm::vec2 mouse_gl_pos = Mouse::getInstance()->getGLPosition();
     glm::vec3 mouse_world_pos = level->calculateWorldPosition(mouse_gl_pos);
-    if (state[SDL_SCANCODE_M]){
-        level->getTerrain()->paintSplatmap(mouse_world_pos);
-    }
-    if (state[SDL_SCANCODE_N]){
-        level->getTerrain()->eraseSplatmap(mouse_world_pos);
-    }
+
     if (state[SDL_SCANCODE_B]){
         level->getTerrain()->paintHeightmap(mouse_world_pos);
     }
     if (state[SDL_SCANCODE_V]){
         level->getTerrain()->eraseHeightmap(mouse_world_pos);
+    }
+
+    if (Mouse::getInstance()->isPressed(Mouse::LEFT)){
+        level->getTerrain()->paintSplatmap(mouse_world_pos);
+    }
+    if (Mouse::getInstance()->isPressed(Mouse::RIGHT)){
+        level->getTerrain()->eraseSplatmap(mouse_world_pos);
     }
 }
 
