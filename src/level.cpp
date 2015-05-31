1edddd08e0ef49427c7df6fc13386a5da3afddfc
Chris Rice <chrisrice93@gmail.com>
2015-05-30 20:12:10 -0600

    Reduced number of units.

--- src/level.cpp
+++ src/level.cpp
@@ -8,9 +8,9 @@ Level::Level(string filename) : unit_holder(), game_map(filename, unit_holder), 
         "shaders/doodad.fs");
     float playable_scale = 1.0f;
 
-    for(int i = 0; i < 10; ++i){
-        for(int j = 0; j < 10; ++j){
-            glm::vec3 playable_position = glm::vec3(3.0f*i, 0.0f, 3.0f*j);
+    for(int i = 0; i < 5; ++i){
+        for(int j = 0; j < 5; ++j){
+            glm::vec3 playable_position = glm::vec3(5 - 3.0f*i, 0.0f, 5 - 3.0f*j);
             Playable temp(playable_mesh, playable_shader, playable_position, playable_scale);
             temp.loadFromXML("res/units/testunit.xml");
             temp.setScale(0.8);
