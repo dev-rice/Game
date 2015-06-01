d0c7e1e4b1660f3cd63d51009811a1f7612c34ce
Chris Rice <chrisrice93@gmail.com>
2015-06-01 17:24:47 -0600

    Shader class written, need to update all the other code from everywhere.

---
+++ src/shader.hpp
@@ -0,0 +1,29 @@
+#ifndef Shader_h
+#define Shader_h
+
+#include <string>
+#include <fstream>
+
+#include "includes/gl.hpp"
+#include "debug.h"
+
+using namespace std;
+
+class Shader {
+public:
+    Shader(string vs_filename, string fs_filename);
+
+    GLuint getGLId();
+private:
+
+    GLuint loadVertexShader(string vs_filename);
+    GLuint loadFragmentShader(string fs_filename);
+    GLuint combineShaderProgram(GLuint vertex_shader, GLuint fragment_shader);
+    GLuint loadShaderProgram(string vs_filename, string fs_filename);
+    string GLSLParse(string);
+
+    GLuint gl_shader_id;
+
+};
+
+#endif
