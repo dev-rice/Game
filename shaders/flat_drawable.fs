e99bf6b86d7c417108eb3464bad4d7e5e6427ec2
Chris Rice <chrisrice93@gmail.com>
2015-06-24 08:55:31 -0600

    Possible fix for all of the pixel perfect errors.

--- shaders/flat_drawable.fs
+++ shaders/flat_drawable.fs
@@ -8,6 +8,7 @@ uniform float opacity;
 uniform sampler2D base_texture;
 
 void main() {
-    vec4 texel = texture(base_texture, Texcoord);
+    vec4 texel = textureOffset(base_texture, Texcoord, ivec2(-0.5 , -0.5));
+
     outColor = vec4(texel.rgb, opacity * texel.a);
 }
